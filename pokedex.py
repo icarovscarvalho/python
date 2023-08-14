0bcb99330262e6f0b3fc8a9a5158ff5bfce9730e
Ícaro Carvalho <123589150+IcaroVSCarvalho@users.noreply.github.com>
2023-08-14 13:59:42 -0300

    update banco de dados

--- pokedex.py
+++ pokedex.py
@@ -1,68 +1,65 @@
-pokemons = {'pikachu': {'nome': 'pikachu',
+#--------------------------------ID:001---------------------------
+pokemons = {'bubasauro': {'nome': 'bubasauro',
+                          'tipo': ('planta', 'veneno')},
+            '1': {'nome': 'bubasauro',
+                  'tipo': ('planta', 'veneno'),
+                  'fraqueza': ('fogo', 'psiquico', 'gelo', 'voador')},
+#--------------------------------ID:002---------------------------
+            'ivysauro': {'nome': 'ivysauro',
+                          'tipo': ('planta', 'veneno')},
+            '2': {'nome': 'ivysauro',
+                  'tipo': ('planta', 'veneno'),
+                  'fraqueza': ('fogo', 'psiquico', 'gelo', 'voador')},
+#--------------------------------ID:003---------------------------
+            'venossauro': {'nome': 'venossauro',
+                         'tipo': ('planta', 'veneno')},
+            '3': {'nome': 'venossauro',
+                  'tipo': ('planta', 'veneno'),
+                  'fraqueza': ('fogo', 'psiquico', 'gelo', 'voador')},
+#--------------------------------ID:004---------------------------
+            'charmander': {'nome': 'charmander',
+                           'tipo': ('planta', 'veneno'),
+                           'fraqueza': ('agua', 'terrestre', 'pedra')},
+            '4': {'nome': 'charmander',
+                  'tipo': 'fogo',
+                  'fraqueza': ('agua', 'terrestre', 'pedra')},
+#--------------------------------ID:005---------------------------
+            'charmeleon': {'nome': 'charmeleon',
+                           'tipo': ('planta', 'veneno'),
+                           'fraqueza': ('agua', 'terrestre', 'pedra')},
+            '5': {'nome': 'charmeleon',
+                  'tipo': 'fogo',
+                  'fraqueza': ('agua', 'terrestre', 'pedra')},
+#--------------------------------ID:006---------------------------
+            'charizard': {'nome': 'charizard',
+                           'tipo': ('planta', 'veneno'),
+                           'fraqueza': ('agua', 'terrestre', 'pedra')},
+            '6': {'nome': 'charizard',
+                  'tipo': 'fogo',
+                  'fraqueza': ('agua', 'terrestre', 'pedra')},
+#--------------------------------ID:024---------------------------
+            'pikachu': {'nome': 'pikachu',
                         'tipo': 'eletrico'},
             '24': {'nome': 'pikachu',
                    'tipo': 'eletrico'},
-
+#--------------------------------ID:132---------------------------
             'eevee': {'nome': 'eevee',
-                      'tipo': 'normal'}
+                      'tipo': 'normal'},
             }
 
-bubasauro = {'numero': 1,
-             'nome': 'bubasauro',
-             'tipo': ('planta', 'veneno'),
-             'fraqueza': ('fogo', 'psiquico', 'gelo', 'voador'),
-             'primeira_evolucao:': 'bubasauro',
-             'segunda_evolucao': 'ivysauro',
-             'terceira_evolucao': 'venossauro'}
+while True:
+    busca = input(f'Escolha um pokemon ou digite o ID: ').lower().strip()
+    if busca not in pokemons:
+        print('Você deve escolher um ID de 1 à 151 ou digitar o nome do Pokemon corretamente!\n')
+    # print(busca)
+    if busca in pokemons:
+        print(pokemons.get(busca))
+        # sair = str(input('Quer escolher outro pokemon? [Y]/[N] ')).lower().strip()
+        # print(sair)
 
-ivysauro = {'numero': 2,
-            'tipo': ('planta', 'veneno'),
-            'fraqueza': ('fogo', 'psiquico', 'gelo', 'voador'),
-            'primeira_evolucao:': 'bubasauro',
-            'segunda_evolucao': 'ivysauro',
-            'terceira_evolucao': 'venossauro'}
-
-venosauro = {'numero': 3,
-             'tipo': ('planta', 'veneno'),
-             'fraqueza': ('fogo', 'psiquico', 'gelo', 'voador'),
-             'primeira_evolucao:': 'bubasauro',
-             'segunda_evolucao': 'ivysauro',
-             'terceira_evolucao': 'venossauro'}
-
-charmander = {'numero': 4,
-              'tipo': 'fogo',
-              'fraqueza': ('agua', 'terrestre', 'pedra'),
-              'primeira_evolucao:': 'charmander',
-              'segunda_evolucao': 'charmeleon',
-              'terceira_evolucao': 'charizard'}
-
-charmeleon = {'numero': 5,
-              'tipo': 'fogo',
-              'fraqueza': ('agua', 'terrestre', 'pedra'),
-              'primeira_evolucao:': 'charmander',
-              'segunda_evolucao': 'charmeleon',
-              'terceira_evolucao': 'charizard'}
-
-charizard = {'numero': 6,
-             'tipo': 'fogo',
-             'fraqueza': ('agua', 'terrestre', 'pedra'),
-             'primeira_evolucao:': 'charmander',
-             'segunda_evolucao': 'charmeleon',
-             'terceira_evolucao': 'charizard'}
-
-busca = input(f'Escolha um pokemon ou digite o ID: ')
-print(busca)
-if busca in pokemons:
-    print(pokemons.get(busca))
-
-# busca = input('Escolha um pokemon: ')
-# print(busca)
-# if busca in pokemons:
-#     for i in pokemons.get(busca):
-#         print(pokemons.get(i))
-
-# busca = input('Escolha um pokemon: ')
-# print(busca)
-# if busca in pokemons:
-#     for i in pokemons:
-#         print(pokemons.get(i))
+        # if sair == 'y':
+        #     continue
+        # elif sair == 'n':
+        #     break
+        # else:
+        #     print('Digite um valor válido!')
